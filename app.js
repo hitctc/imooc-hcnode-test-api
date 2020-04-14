42e60e7a85fdb06455744e2c6353df5ec00333d4
huachuan <hc.5@qq.com>
2020-04-14 17:58:42 +0800

    准备前后端联调，因此拷贝前端项目；

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-//app.js
+import {Token} from '/models/token.js'
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
+     const token = new Token()
+     token.verify()
   }
 })
+
+//主题 渐进式  核心主要
+// 抽象概念 思考 Model
+
+// 实践性非常强
+// 业务
+// 承载知识
+
+// 编程的思维 NodeJS KOA 7
+// 线上API Python Flask
+// JavaScript 独立开发完整小程序
+// JavaScript 语言本身的深度进阶
+
+// Python Flask 
+// Java SprintBoot
+// NodeJS KOA
+
+// 语法简洁 JavaScript ES2019 JS Python
+// 原型链 工程化 OO
+
+// 面向对象
+
+// function
+// 优雅 
+
+// 使用场景 javascript > Python
+// 浏览器 读取文件
+
+// JS 脱离 python js
+// nodejs API 网站
+
+// 异步 flask django
+// KOA async await express
+// 资源 http 异步 操作数据库
